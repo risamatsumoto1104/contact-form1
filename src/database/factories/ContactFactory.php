37ef10f83d0060f0a9fe7758a7e4fbc2b82d3e68
risamatsumoto1104 <risashibata114@gmail.com>
2024-11-01 22:39:39 +0900

    241101_22:39_ダミーデータ作成

---
+++ src/database/factories/ContactFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Contact;
+
+class ContactFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'category_id' => $this->faker->numberBetween(1, 100), 
+            'first_name' => $this->faker->firstName,
+            'last_name' => $this->faker->lastName,
+            'gender' => $this->faker->randomElement(['男性', '女性','その他']),
+            'email' => $this->faker->unique()->safeEmail,
+            'tell' => $this->faker->phoneNumber,
+            'address' => $this->faker->address,
+            'building' => $this->faker->secondaryAddress,
+            'detail' => $this->faker->text(20)
+        ];
+    }
+}
